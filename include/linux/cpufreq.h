1ca2830c77d91d861972eebc035d0bae65dd4f95
pec0ra <heraclez@hotmail.com>
2014-05-06 19:29:00 +0200

    Added Smatass and Smoothass governors

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSV2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass2)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS)
+extern struct cpufreq_governor cpufreq_gov_smartass;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartass)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMOUTHASS)
+extern struct cpufreq_governor cpufreq_gov_smouthass;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smouthass)
 #endif
 
 
