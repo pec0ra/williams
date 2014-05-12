83163ac6dd253b59195ef7bfbe443fd0f4cfe1fd
pec0ra <heraclez@hotmail.com>
2014-05-12 09:44:18 +0200

    Reverse SmartassV2 tuning

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -62,7 +62,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP (220*1000)
+#define DEFAULT_RAMP_UP_STEP (384*1000)
 static unsigned int ramp_up_step;
 
 /*
